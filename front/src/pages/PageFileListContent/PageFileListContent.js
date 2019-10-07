fbed0eec1bd0a0d870411fc8098778a8dbd73c3c
fosemberg <fosemberg@gmail.com>
2019-10-07 13:48:13 +0300

    refactoring theme

--- front/src/pages/PageFileListContent/PageFileListContent.js
+++ front/src/pages/PageFileListContent/PageFileListContent.js
@@ -11,63 +11,66 @@ import BranchInfoInfo from "../../components/BranchInfo/-Info/BranchInfo-Info";
 import TabsItem from "../../components/Tabs/-Item/Tabs-Item";
 import Tabs from "../../components/Tabs/Tabs";
 import Layout from "../../components/Layout/Layout";
+import Theme from "../../components/Theme/Theme";
 
 const PageFileListContent = () => (
-    <Layout mix={{ 'block': 'theme', 'mods': { 'space': 'default', 'size': 'default', 'color': 'project-default', 'gap': 'small', 'font': 'default' } }}>
-        <header/>
-        <LayoutContainer grow='true'>
-            <BreadCrumbs borderB={true}>
-                <BreadCrumbsItem>
-                    arcadia
-                </BreadCrumbsItem>
-            </BreadCrumbs>
-            <BranchInfo border='bottom'>
-                <BranchInfoHeader>
-                    <BranchInfoName>
-                        arcadia
-                    </BranchInfoName>
-                    <BranchInfoSelect mix={{ 'block': 'select', 'mods': { 'size': 'big' } }}>
+  <Theme space='default' size='default' color='project-default' gap='small' font='default'>
+    <Layout>
+      <header/>
+      <LayoutContainer grow='true'>
+        <BreadCrumbs borderB={true}>
+          <BreadCrumbsItem>
+            arcadia
+          </BreadCrumbsItem>
+        </BreadCrumbs>
+        <BranchInfo border='bottom'>
+          <BranchInfoHeader>
+            <BranchInfoName>
+              arcadia
+            </BranchInfoName>
+            <BranchInfoSelect mix={{'block': 'select', 'mods': {'size': 'big'}}}>
 <span color='secondary'>
 trunk
 </span>
-                        <i position='baseline' mix={{ 'block': 'arrow', 'mods': { 'state': 'down', 'color': 'secondary' } }}/>
-                    </BranchInfoSelect>
-                    <BranchInfoSearch>
-                    </BranchInfoSearch>
-                </BranchInfoHeader>
-                <BranchInfoInfo>
-                    {' Last commit '}
-                    <a attrs={{ 'href': '#' }}>
-                        c4d248
-                    </a>
-                    {' on '}
-                    <a attrs={{ 'href': '#' }}>
-                        20 Oct 2017, 12:24
-                    </a>
-                    {' by '}
-                    <span>
+              <i position='baseline' mix={{'block': 'arrow', 'mods': {'state': 'down', 'color': 'secondary'}}}/>
+            </BranchInfoSelect>
+            <BranchInfoSearch>
+            </BranchInfoSearch>
+          </BranchInfoHeader>
+          <BranchInfoInfo>
+            {' Last commit '}
+            <a attrs={{'href': '#'}}>
+              c4d248
+            </a>
+            {' on '}
+            <a attrs={{'href': '#'}}>
+              20 Oct 2017, 12:24
+            </a>
+            {' by '}
+            <span>
 robot-srch-releaser
 </span>
-                </BranchInfoInfo>
-                <Tabs>
-                    <TabsItem state='active'>
-                        files
-                    </TabsItem>
-                    <TabsItem>
-                        branches
-                    </TabsItem>
-                </Tabs>
-            </BranchInfo>
-            <table hide='touch'>
-                <thead>
-                <tr/>
-                </thead>
-                <tbody mix={{ 'block': 'desktop-files' }}/>
-            </table>
+          </BranchInfoInfo>
+          <Tabs>
+            <TabsItem state='active'>
+              files
+            </TabsItem>
+            <TabsItem>
+              branches
+            </TabsItem>
+          </Tabs>
+        </BranchInfo>
+        <table hide='touch'>
+          <thead>
+          <tr/>
+          </thead>
+          <tbody mix={{'block': 'desktop-files'}}/>
+        </table>
 
-        </LayoutContainer>
-        <footer/>
+      </LayoutContainer>
+      <footer/>
     </Layout>
+  </Theme>
 );
 
 export default PageFileListContent;
