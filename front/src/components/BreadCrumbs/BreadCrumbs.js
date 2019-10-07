f2b923a59c74f696ebab4eed2e732a29ed0e97a8
fosemberg <fosemberg@gmail.com>
2019-10-08 00:09:22 +0300

    working breadCrumbs

--- front/src/components/BreadCrumbs/BreadCrumbs.js
+++ front/src/components/BreadCrumbs/BreadCrumbs.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import {cn} from "@bem-react/classname";
+import {Route} from 'react-router-dom';
 import './BreadCrumbs.scss';
+import BreadCrumbsItem from "./-Item/BreadCrumbs-Item";
 
 export const cnBreadCrumbs = cn('BreadCrumbs');
 
-const BreadCrumbs = ({className, children}) => (
-  <div className={cnBreadCrumbs({}, [className])}>{children}</div>
-);
+const BreadCrumbs = () =>
+    <Route
+        path="*"
+        render={({location: {pathname}}) =>
+            <div className={cnBreadCrumbs()}>
+                {
+                    pathname
+                        .split("/")
+                        .map((crumb, key) =>
+                            <BreadCrumbsItem {...{key}}>{crumb}</BreadCrumbsItem>)
+                }
+            </div>
+        }
+    />;
 
 export default BreadCrumbs;
--- front/src/pages/App/App.js
+++ front/src/pages/App/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.scss';
 import PageFileListContent from "../PageFileListContent/PageFileListContent";
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => (
+    <BrowserRouter>
     <PageFileListContent/>
+    </BrowserRouter>
 );
 
 export default App;
