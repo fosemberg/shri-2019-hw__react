4909e3f1e404368e2794292f95f47dc6d19c4b27
fosemberg <fosemberg@gmail.com>
2019-10-08 11:45:36 +0300

    Files to external component

---
+++ front/src/components/Files/Files.js
@@ -0,0 +1,170 @@
+import React from "react";
+import TableRow from "../../patterns/Table/-Row/Table-Row";
+import TableCell from "../../patterns/Table/-Cell/Table-Cell";
+import {Link} from "react-router-dom";
+import {withRouter} from 'react-router-dom';
+import {cnLink} from "../../patterns/Link/Link";
+import "../../patterns/Link/Link.scss";
+import {cnFile} from "../../patterns/File/File";
+import "../../patterns/File/File.scss";
+import "../../patterns/File/_type/File_type_branch.scss";
+import "../../patterns/File/_type/File_type_dir.scss";
+import "../../patterns/File/_type/File_type_file.scss";
+
+const dataMock = [
+  {
+    "num": "100644",
+    "fileType": "file",
+    "lastCommit": "79d57a",
+    "name": ".gitignore",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  },
+  {
+    "num": "100644",
+    "fileType": "file",
+    "lastCommit": "2f3ca9",
+    "name": "API.md",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  },
+  {
+    "num": "100644",
+    "fileType": "file",
+    "lastCommit": "ddfd03",
+    "name": "README.md",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  },
+  {
+    "num": "040000",
+    "fileType": "dir",
+    "lastCommit": "0fb9f0",
+    "name": "README",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  },
+  {
+    "num": "100644",
+    "fileType": "file",
+    "lastCommit": "3f287e",
+    "name": "images.d.ts",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  },
+  {
+    "num": "100644",
+    "fileType": "file",
+    "lastCommit": "1c360e",
+    "name": "package.json",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  },
+  {
+    "num": "040000",
+    "fileType": "dir",
+    "lastCommit": "461e8d",
+    "name": "public",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  },
+  {
+    "num": "040000",
+    "fileType": "dir",
+    "lastCommit": "e34870",
+    "name": "src",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  },
+  {
+    "num": "100644",
+    "fileType": "file",
+    "lastCommit": "7d74db",
+    "name": "tsconfig.json",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  },
+  {
+    "num": "100644",
+    "fileType": "file",
+    "lastCommit": "414421",
+    "name": "tsconfig.prod.json",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  },
+  {
+    "num": "100644",
+    "fileType": "file",
+    "lastCommit": "65ffdd",
+    "name": "tsconfig.test.json",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  },
+  {
+    "num": "100644",
+    "fileType": "file",
+    "lastCommit": "206b22",
+    "name": "tslint.json",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  },
+  {
+    "num": "100644",
+    "fileType": "file",
+    "lastCommit": "74373a",
+    "name": "yarn.lock",
+    "commitMessage": "[vcs] move http to arc",
+    "committer": "noxoomo",
+    "updated": "4 s ago"
+  }
+]
+
+const Files = withRouter(({location}) => {
+  let {pathname} = location;
+  console.log(pathname);
+  pathname = pathname.slice(-1) === '/' ? pathname : pathname + '/';
+  return <>
+    {
+      dataMock.map(
+        ({
+           fileType,
+           name,
+           lastCommit,
+           commitMessage,
+           committer,
+           updated
+         }, key) =>
+          <TableRow key={key}>
+            <TableCell>
+              <Link to={`${pathname}${name}`} className={cnFile({type: 'dir'}, [cnLink()])}>
+                <div className="file__icon file__icon_type_${fileType}"></div>
+                {name}
+              </Link>
+            </TableCell>
+            <TableCell>
+              <a className="link link__control" href="#" role="link"
+                 tabindex="0">{lastCommit}
+              </a>
+            </TableCell>
+            <TableCell>{commitMessage}</TableCell>
+            <TableCell><span className="user">{committer}</span></TableCell>
+            <TableCell>{updated}</TableCell>
+          </TableRow>
+      )
+    }
+  </>
+});
+
+export default Files;
--- front/src/pages/PageFileListContent/PageFileListContent.js
+++ front/src/pages/PageFileListContent/PageFileListContent.js
@@ -26,247 +26,90 @@ import TableCell from "../../patterns/Table/-Cell/Table-Cell";
 import TableHead from "../../patterns/Table/-Head/Table-Head";
 import Table from "../../patterns/Table/Table";
 import {BranchInfoBorderBottom} from "../../patterns/BranchInfo/_border/BranchInfo_border_bottom";
-import {withRouter} from 'react-router-dom';
-import {cnLink} from "../../patterns/Link/Link";
-import "../../patterns/Link/Link.scss";
-import {cnFile} from "../../patterns/File/File";
-import "../../patterns/File/File.scss";
-import "../../patterns/File/_type/File_type_branch.scss";
-import "../../patterns/File/_type/File_type_dir.scss";
-import "../../patterns/File/_type/File_type_file.scss";
 import BreadCrumbs from "../../components/BreadCrumbs/BreadCrumbs";
+import Files from "../../components/Files/Files";
 
 const Theme = compose(
-    ThemeSpaceDefault,
-    ThemeSizeDefault,
-    ThemeColorProjectDefault,
-    ThemeGapSmall
+  ThemeSpaceDefault,
+  ThemeSizeDefault,
+  ThemeColorProjectDefault,
+  ThemeGapSmall
 )(ThemeBase);
 
 const LayoutContainer = compose(
-    LayoutContainerGrow
+  LayoutContainerGrow
 )(LayoutContainerBase)
 
 const TabsItem = compose(
-    TabsItemStateActive
+  TabsItemStateActive
 )(TabsItemBase)
 
 const BranchInfo = compose(
-    BranchInfoBorderBottom
+  BranchInfoBorderBottom
 )(BranchInfoBase)
 
 const headerMock = [
-    'name',
-    'Last commit',
-    'Commit message',
-    'Committer',
-    'Updated'
+  'name',
+  'Last commit',
+  'Commit message',
+  'Committer',
+  'Updated'
 ]
 
-const dataMock = [
-    {
-        "num": "100644",
-        "fileType": "file",
-        "lastCommit": "79d57a",
-        "name": ".gitignore",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    },
-    {
-        "num": "100644",
-        "fileType": "file",
-        "lastCommit": "2f3ca9",
-        "name": "API.md",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    },
-    {
-        "num": "100644",
-        "fileType": "file",
-        "lastCommit": "ddfd03",
-        "name": "README.md",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    },
-    {
-        "num": "040000",
-        "fileType": "dir",
-        "lastCommit": "0fb9f0",
-        "name": "README",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    },
-    {
-        "num": "100644",
-        "fileType": "file",
-        "lastCommit": "3f287e",
-        "name": "images.d.ts",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    },
-    {
-        "num": "100644",
-        "fileType": "file",
-        "lastCommit": "1c360e",
-        "name": "package.json",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    },
-    {
-        "num": "040000",
-        "fileType": "dir",
-        "lastCommit": "461e8d",
-        "name": "public",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    },
-    {
-        "num": "040000",
-        "fileType": "dir",
-        "lastCommit": "e34870",
-        "name": "src",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    },
-    {
-        "num": "100644",
-        "fileType": "file",
-        "lastCommit": "7d74db",
-        "name": "tsconfig.json",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    },
-    {
-        "num": "100644",
-        "fileType": "file",
-        "lastCommit": "414421",
-        "name": "tsconfig.prod.json",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    },
-    {
-        "num": "100644",
-        "fileType": "file",
-        "lastCommit": "65ffdd",
-        "name": "tsconfig.test.json",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    },
-    {
-        "num": "100644",
-        "fileType": "file",
-        "lastCommit": "206b22",
-        "name": "tslint.json",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    },
-    {
-        "num": "100644",
-        "fileType": "file",
-        "lastCommit": "74373a",
-        "name": "yarn.lock",
-        "commitMessage": "[vcs] move http to arc",
-        "committer": "noxoomo",
-        "updated": "4 s ago"
-    }
-]
-
-
-const PageFileListContent = withRouter(({location}) => {
-    let {pathname} = location;
-    pathname = pathname.slice(-1) === '/' ? pathname : pathname + '/';
-    return <Theme space='default' size='default' color='project-default' gap='small' font='default'>
-        <Layout>
-            <Header/>
-            <LayoutContainer grow>
-                <BreadCrumbs/>
-                <BranchInfo border='bottom'>
-                    <BranchInfoHeader>
-                        <BranchInfoName>
-                            arcadia
-                        </BranchInfoName>
-                        <BranchInfoSelect mix={{'block': 'select', 'mods': {'size': 'big'}}}>
+const PageFileListContent = () => (
+  <Theme space='default' size='default' color='project-default' gap='small' font='default'>
+    <Layout>
+      <Header/>
+      <LayoutContainer grow>
+        <BreadCrumbs/>
+        <BranchInfo border='bottom'>
+          <BranchInfoHeader>
+            <BranchInfoName>
+              arcadia
+            </BranchInfoName>
+            <BranchInfoSelect mix={{'block': 'select', 'mods': {'size': 'big'}}}>
 <span color='secondary'>
 trunk
 </span>
-                            <i position='baseline'
-                               mix={{'block': 'arrow', 'mods': {'state': 'down', 'color': 'secondary'}}}/>
-                        </BranchInfoSelect>
-                        <BranchInfoSearch>
-                        </BranchInfoSearch>
-                    </BranchInfoHeader>
-                    <BranchInfoInfo>
-                        {' Last commit '}
-                        <Link>c4d248</Link>
-                        {' on '}
-                        <Link>20 Oct 2017, 12:24</Link>
-                        {' by '}
-                        <User>robot-srch-releaser</User>
-                    </BranchInfoInfo>
-                    <Tabs>
-                        <TabsItem state='active'>
-                            files
-                        </TabsItem>
-                        <TabsItem>
-                            branches
-                        </TabsItem>
-                    </Tabs>
-                </BranchInfo>
-                <Table hide='touch'>
-                    <TableHead>
-                        <TableRow>
-                            {headerMock.map(headerName =>
-                                <TableCell key={headerName}>{headerName}</TableCell>
-                            )}
-                        </TableRow>
-                    </TableHead>
-                    <tbody>
-                    {
-                        dataMock.map(
-                            ({
-                                 fileType,
-                                 name,
-                                 lastCommit,
-                                 commitMessage,
-                                 committer,
-                                 updated
-                             }, key) =>
-                                <TableRow key={key}>
-                                    <TableCell>
-                                        <Link to={`${pathname}${name}`} className={cnFile({type: 'dir'},[cnLink()])}>
-                                            <div className="file__icon file__icon_type_${fileType}"></div>
-                                            {name}
-                                        </Link>
-                                    </TableCell>
-                                    <TableCell>
-                                        <a className="link link__control" href="#" role="link"
-                                           tabindex="0">{lastCommit}
-                                        </a>
-                                    </TableCell>
-                                    <TableCell>{commitMessage}</TableCell>
-                                    <TableCell><span className="user">{committer}</span></TableCell>
-                                    <TableCell>{updated}</TableCell>
-                                </TableRow>
-                        )
-                    }
-                    </tbody>
-                </Table>
-            </LayoutContainer>
-            <Footer/>
-        </Layout>
-    </Theme>
-});
+              <i position='baseline'
+                 mix={{'block': 'arrow', 'mods': {'state': 'down', 'color': 'secondary'}}}/>
+            </BranchInfoSelect>
+            <BranchInfoSearch>
+            </BranchInfoSearch>
+          </BranchInfoHeader>
+          <BranchInfoInfo>
+            {' Last commit '}
+            <Link>c4d248</Link>
+            {' on '}
+            <Link>20 Oct 2017, 12:24</Link>
+            {' by '}
+            <User>robot-srch-releaser</User>
+          </BranchInfoInfo>
+          <Tabs>
+            <TabsItem state='active'>
+              files
+            </TabsItem>
+            <TabsItem>
+              branches
+            </TabsItem>
+          </Tabs>
+        </BranchInfo>
+        <Table hide='touch'>
+          <TableHead>
+            <TableRow>
+              {headerMock.map(headerName =>
+                <TableCell key={headerName}>{headerName}</TableCell>
+              )}
+            </TableRow>
+          </TableHead>
+          <tbody>
+          <Files/>
+          </tbody>
+        </Table>
+      </LayoutContainer>
+      <Footer/>
+    </Layout>
+  </Theme>
+)
 
 export default PageFileListContent;
