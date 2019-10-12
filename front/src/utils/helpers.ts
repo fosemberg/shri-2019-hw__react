32b7fa240ee3989bfed2dbf7f878d57f52f052d5
fosemberg <fosemberg@gmail.com>
2019-10-12 21:48:50 +0300

    add typescript

--- front/src/utils/helpers.ts
+++ front/src/utils/helpers.ts
@@ -1,19 +1,19 @@
 import {useLocation} from "react-router";
 
-export const usePathname = () => {
+export const usePathname = (): string => {
   const {pathname} = useLocation();
   return pathname.slice(-1) === '/' ? pathname : pathname + '/';
-}
+};
 
-export const usePathnameArray = () => {
+export const usePathnameArray = (): string[] => {
   return urlToArray(useLocation().pathname);
-}
+};
 
-export const urlToArray = url => url.split('/').filter(x => x !== '');
-export const arrayToUrl = array => array.join('/');
+export const urlToArray = (url: string): string[] => url.split('/').filter(x => x !== '');
+export const arrayToUrl = (array: string[]): string => array.join('/');
 
-export const generateGetFileUrl = (url) => {
+export const generateGetFileUrl = (url: string): string => {
   const urlArr = urlToArray(url);
   const repository = urlArr.shift();
   return ['', repository, 'blob/master', ...urlArr].join('/');
-}
+};
