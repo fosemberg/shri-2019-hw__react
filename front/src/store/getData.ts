ad5867ea3b19bed9119601ab642f093091b96f8d
fosemberg <fosemberg@gmail.com>
2019-10-12 22:18:26 +0300

    getData

--- front/src/store/getData.ts
+++ front/src/store/getData.ts
@@ -1,9 +1,9 @@
 import {generateGetFileUrl, urlToArray} from "../utils/helpers";
-import {FileType} from "../utils/types";
+import {FileType, Url} from "../utils/types";
 
-const getData = (url) => {
-  const urlArr = urlToArray(url);
-  const target = urlArr.pop();
+const getData = (url: Url): Promise<Object> => {
+  const urlArr: Array<string> = urlToArray(url);
+  const target:string | undefined = urlArr.pop();
   const fileType = target && ~target.indexOf('.') ? FileType.file : FileType.dir;
   const prefix = 'http://localhost:3009/api/repos';
 
@@ -12,6 +12,6 @@ const getData = (url) => {
     : url;
   _url = `${prefix}${_url}`;
   return fetch(`${_url}`).then(res => res.json());
-}
+};
 
 export default getData;
